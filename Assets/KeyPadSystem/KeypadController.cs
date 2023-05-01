7504abfd591e305ad509d39816d8a1552af765ac
dbhargavQU <99452015+dbhargavQU@users.noreply.github.com>
2023-05-01 11:45:36 -0400

    Level3Updates

---
+++ Assets/KeyPadSystem/KeypadController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeypadController : MonoBehaviour
+{
+    public string password;
+    public int passwordLimit;
+    public Text passwordText;
+    public Animator doorAnimator; // reference to the Animator component on the door GameObject
+
+    //[Header("SFX")]
+    //public AudioClip SafeOpenSound;
+
+    private void Start()
+    {
+        passwordText.text = "";
+    }
+
+    public void PasswordEntry(string number)
+    {
+        if (number == "Clear")
+        {
+            Clear();
+            return;
+        }
+        else if(number == "Enter")
+        {
+            Enter();
+            return;
+        }
+
+        int length = passwordText.text.ToString().Length;
+        if(length<passwordLimit)
+        {
+            passwordText.text = passwordText.text + number;
+        }
+    }
+
+    public void Clear()
+    {
+        passwordText.text = "";
+        passwordText.color = Color.white;
+    }
+
+    private void Enter()
+    {
+        if (passwordText.text == password)
+        {
+            Debug.Log("Correct password entered!");
+            passwordText.color = Color.green;
+            StartCoroutine(waitAndClear());
+
+            // play the animation on the door GameObject
+            doorAnimator.Play("Open");
+
+            //Play sound
+            //AudioManager.PlaySound(SafeOpenSound, 6f);
+        }
+        else
+        {
+            Debug.Log("Incorrect password entered.");
+            passwordText.color = Color.red;
+            StartCoroutine(waitAndClear());
+        }
+    }
+
+    IEnumerator waitAndClear()
+    {
+        yield return new WaitForSeconds(0.75f);
+        Clear();
+    }
+}
